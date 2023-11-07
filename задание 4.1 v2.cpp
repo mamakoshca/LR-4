7364cd18904be4729a39eb2f2a3aac2fb7bbce04
mamakoshca <enotartur5@gmail.com>
2023-11-07 17:26:43 +0700

    LR-4
    LR-4

---
+++ задание 4.1 v2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+/*
+Вариант 16 Дано вещественное число B, целое число N и набор из N вещественных чисел,
+упорядоченных по возрастанию.Вывести элементы
+набора вместе с числом B, сохраняя упорядоченность выводимых чисел.
+*/
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	double b, x;
+	int n;
+	cout << "Введите b:";
+	cin >> b;
+	cout << "Введите n:";
+	cin >> n;
+	while (n > 0) {
+		cout << "Введите число:";
+		cin >> x;
+		n = n - 1;
+		if (x > b) {
+			cout << b << endl;
+			cout << x << endl;
+			break;
+		}
+		cout << x << endl;
+	}
+	while (n > 0) {
+		cout << "Введите число:";
+		cin >> x;
+		n = n - 1;
+		cout << x << endl;
+	}
+}
---
+++ задание 4.2 v2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+/*
+Задание 16. Дано вещественное число A и целое число N (> 0).
+Используя один цикл, вывести все целые степени числа A от 1 до N.
+*/
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	double a,x=1.0;
+	int n;
+	cout << "Введите А:";
+	cin >> a;
+	cout << "Введите N:";
+	cin >> n;
+	for (int i = 1; i <= n; ++i) {
+		x*=a;
+		cout << x << endl;
+	}
+}
